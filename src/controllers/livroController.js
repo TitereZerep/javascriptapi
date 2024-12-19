aed5eaaa7d8816d02e2f91f19d4e6c6833b71188
TitereZerep <victorperezveiga@gmail.com>
2024-12-18 23:19:42 -0300

    controller

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,20 +3,42 @@ import livro from "../config/models/Livro.js"
 class LivroController {
 
     static async listarLivros(req, res) {
-        const listaLivros = await livro.find ({});
-        res.status(200).json(listaLivros);
-    }
-
-    static async cadastrarLivro(req, res){
         try {
-        const novoLivro = await livro.create(req.body);
-        res.status(201).json({message: "criado com sucesso", livro: novoLivro});
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
         } catch (erro) {
-            res.status(500).json({message: `${erro.message} - essa porra deu erro`})
+            res.status(500).json({ message: `${erro.message} - deu erro nesse caralho aqui` });
         }
-    }
-    
+    };
 
+    static async listarLivroPorId(req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado)
+} catch(erro) {
+        res.status(500).json({ message: `${erro.message} - essa porra deu erro` })
+    }
+}
+    
+    static async cadastrarLivro(req, res) {
+    try {
+        const novoLivro = await livro.create(req.body);
+        res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
+    } catch (erro) {
+        res.status(500).json({ message: '${erro.message} - falha ao cadastrar livro' });
+    }
+}
+
+    static async atualizarLivro(req, res){
+    try {
+        const id = req.params.id;
+        await livro.findByIdAndUpdate(id, req.body);
+        res.status(200).json({ message: "livro atualizado" });
+    } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha na atualização` })
+    }
+};
 };
 
 export default LivroController;
